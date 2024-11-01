cb5d5beac6a33071699d9be1d5adcb5179115159
Mike Meadows <mmeadows@waypathconsulting.com>
2024-11-01 10:12:48 -0400

    see if no capitilzation works

--- build/install_Salesforce_Cli.sh
+++ build/install_Salesforce_Cli.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+# Simple script to log "Hello" with a timestamp
 
+
+# Append "Hello" with the current date and time to the log file
 echo "<<<<<<<<<<<   $(date): Hello From CircleCI >>>>>>>>>>>"
