d23079ed3fd2676205bfa3b41ec7016d72ebf6dd
Mike Meadows <mmeadows@waypathconsulting.com>
2024-11-02 12:28:04 -0400

    got rid of non existant flag

--- build/run_sfdx_scanner.sh
+++ build/run_sfdx_scanner.sh
@@ -6,4 +6,4 @@ echo "Install SFDX Scanner"
 echo -e 'y/n' | sf plugins:install @salesforce/sfdx-scanner@4.6.0
 
 echo "Running SFDX Scanner"
-npx sfdx scanner:run --target "**/default/**" --format "csv" --outfile "sfdxScannerAnalysis.csv" --violations-cause-error
+npx sfdx scanner:run --target "**/default/**" --format "csv" --outfile "sfdxScannerAnalysis.csv"
