9db001afef91a72946f648c500a90f234154f1b6
Mike Meadows <mmeadows@waypathconsulting.com>
2024-11-02 12:22:18 -0400

    changed the scanner version

--- build/run_sfdx_scanner.sh
+++ build/run_sfdx_scanner.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install openjdk-11-jdk
 
 echo "Install SFDX Scanner"
-echo -e 'y/n' | sfdx plugins:install @salesforce/sfdx-scanner
+echo -e 'y/n' | sf plugins:install @salesforce/sfdx-scanner@4.6.0
 
 echo "Running SFDX Scanner"
 npx sfdx scanner:run --target "**/default/**" --format "csv" --outfile "sfdxScannerAnalysis.csv" --violations-cause-error
