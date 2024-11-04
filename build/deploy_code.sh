295c9bc15ea90c7a561f0d60acaeeeaf13fd0245
Mike Meadows <mmeadow33@gmail.com>
2024-11-04 11:15:59 -0500

    trying to deploy to scratch org

--- build/deploy_code.sh
+++ build/deploy_code.sh
@@ -5,4 +5,9 @@ sf project deploy repo convert --output-dir deploy_code --source-dir force-app
 
 echo "Deploying code to org"
 ##sfdx force:mdapi:deploy -u mikesPeronalOrg -d deploy_code/ -w -1 -l RunLocalTests
-sf project deploy start --target-org mikesPeronalOrg --deploy-dir deploy_code/ --wait -1 --test-level RunLocalTests
+##sf project deploy start --target-org mikesPeronalOrg --deploy-dir deploy_code/ --wait -1 --test-level RunLocalTests
+
+
+
+#deploy to scratch org
+sf project deploy start --target-org MyScratchOrg --source-dir force-app
--- build/run_apex_tests.sh
+++ build/run_apex_tests.sh
@@ -8,3 +8,6 @@ echo "Testing code in org"
 ## --test-level RunLocalTests --output-dir test-results --result-format tap --target-org mikesPeronalOrg
 sf apex run test --test-level RunLocalTests --output-dir test-results --target-org mikesPeronalOrg     ### this is failing due to too many test exections in 24 hours, https://salesforce.stackexchange.com/questions/280419/failed-to-enqueue-tests-an-unknown-exception-occurred
 ####Error (1): An unknown exception occurred.
+
+##To check this use the REST explorer on Workbench and query GET /services/data/v57.0/limits
+#then look for DailyAsyncApexTests
