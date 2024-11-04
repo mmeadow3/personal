69003f4097ffc1f6338ecde221fabb742000d33a
Mike Meadows <mmeadows@waypathconsulting.com>
2024-11-04 08:22:34 -0500

    now this should pass since the testing limits for SF have gone down

--- build/run_apex_tests.sh
+++ build/run_apex_tests.sh
@@ -6,4 +6,5 @@ sf project deploy start --manifest manifest/package.xml --target-org mikesPerona
 echo "Testing code in org"
 # old sfdx cli:    sfdx force:apex:test:run --testlevel RunLocalTests --outputdir test-results --resultformat tap --targetusername mikesPeronalOrg
 ## --test-level RunLocalTests --output-dir test-results --result-format tap --target-org mikesPeronalOrg
-sf apex run test --test-level RunLocalTests --output-dir test-results --target-org mikesPeronalOrg
+sf apex run test --test-level RunLocalTests --output-dir test-results --target-org mikesPeronalOrg     ### this is failing due to too many test exections in 24 hours, https://salesforce.stackexchange.com/questions/280419/failed-to-enqueue-tests-an-unknown-exception-occurred
+####Error (1): An unknown exception occurred.
