f376b388c50b9ec4dda11dedb6571140a59bbed3
Mike Meadows <mmeadow33@gmail.com>
2024-11-04 11:28:21 -0500

    changed sf command

--- build/deploy_code.sh
+++ build/deploy_code.sh
@@ -1,6 +1,8 @@
 echo "Coverting source to metadata format"
 ##sfdx force:source:convert -d deploy_code -r force-app
-sf project deploy repo convert --output-dir deploy_code --source-dir force-app
+##sf project deploy repo convert --output-dir deploy_code --source-dir force-app
+
+sf project convert mdapi --root-dir force-app --output-dir deploy_code
 
 
 echo "Deploying code to org"
