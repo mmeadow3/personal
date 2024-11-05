f796c4fbf6dc5eceeb5d96cd7525036faec633d2
Mike Meadows <mmeadows@waypathconsulting.com>
2024-11-05 11:45:53 -0500

    got rid of unused build class

--- build/login_to_org.sh
+++
@@ -1,7 +0,0 @@
-echo "Logging into Salesforce Org"
-mkdir keys
-echo $SANDBOX_CERT_KEY | base64 -di > keys/server.key
-
-echo "Authenticating org"
-####sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --setdefaultdevhubusername -a DevHub
-sf login org jwt --client-id $APP_KEY --jwt-key-file keys/server.key --username $SF_USERNAME --set-default-dev-hub --alias mikesPeronalOrg
